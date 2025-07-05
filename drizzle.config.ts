c2589e8545cbadfad9be2c4f82b930e8635df90a
simplecyberhub <info@simplecyberhub.com>
2025-07-05 09:55:04 -0500

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,24 +4,6 @@ import "dotenv/config";
 import { config } from "dotenv";
 config();
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL is not set. Ensure the database is provisioned.",
-  );
-}
-
-if (!process.env.APP_PORT) {
-  throw new Error(
-    "APP_PORT is not set. Ensure the application port is configured.",
-  );
-}
-
-if (!process.env.LOG_LEVEL) {
-  throw new Error(
-    "LOG_LEVEL is not set. Ensure the log level is configured.",
-  );
-}
-
 if (!process.env.DATABASE_HOST) {
   throw new Error(
     "DATABASE_HOST is not set. Ensure the database host is configured.",
@@ -32,13 +14,11 @@ if (!process.env.DATABASE_PORT) {
     "DATABASE_PORT is not set. Ensure the database port is configured.",
   );
 }
-
 if (!process.env.DATABASE_USER) {
   throw new Error(
     "DATABASE_USER is not set. Ensure the database user is configured.",
   );
 }
-
 if (!process.env.DATABASE_PASSWORD) {
   throw new Error(
     "DATABASE_PASSWORD is not set. Ensure the database password is configured.",
@@ -57,12 +37,9 @@ export default defineConfig({
   dbCredentials: {
     host: process.env.DATABASE_HOST || "",
     port: Number(process.env.DATABASE_PORT || 5432),
-    url: process.env.DATABASE_URL || "",
-    
     user: process.env.DATABASE_USER || "",
     password: process.env.DATABASE_PASSWORD || "",
     database: process.env.DATABASE_NAME || "",
-    ssl: false,        // Disable SSL 
-    
+    ssl: { rejectUnauthorized: false }, // Force SSL, accept self-signed
   },
 });
